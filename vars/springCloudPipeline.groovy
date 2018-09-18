7e887954900d607e24cc2647c557c19899be6e9d
xudong82113 <xudong82113@gmail.com>
2018-09-18 12:13:19 +0800

    fix eureka-jenkinsfile

---
+++ vars/springCloudPipeline.groovy
@@ -0,0 +1,32 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        environment {
+            branch = pipelineParams.branch
+            scmUrl = pipelineParams.scmUrl
+            serviceName = pipelineParams.serviceName
+        }
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: branch, url: scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+
+            stage('deploy'){
+                steps {
+                    sh "JENKINS_NODE_COOKIE=dontKillMe nohup java -jar ${pipelineParams.serviceName}/target/${pipelineParams.serviceName}.jar &"
+                }
+            }
+
+        
+        }
+    
+    }
+}
