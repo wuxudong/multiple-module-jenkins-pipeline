3544646d1402ea853ab232c706d0d28d032a9ae1
xudong82113 <xudong82113@gmail.com>
2018-09-19 00:32:30 +0800

    fix eureka-jenkinsfile

--- src/com/github/wuxudong/pipeline/utils/TernaryUtils.groovy
+++ src/com/github/wuxudong/pipeline/utils/TernaryUtils.groovy
@@ -3,6 +3,7 @@ package com.github.wuxudong.pipeline.utils
 class TernaryUtils {
 
     public static String getOrDefault(value, defaultValue) {
+    	System.out.println("value " + value + "defaultValue " + defaultValue)
         return value ? value : defaultValue
     }
 
