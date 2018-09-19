1b0265e3f2950ea1a7b017dab97c521de19cd3ed
xudong82113 <xudong82113@gmail.com>
2018-09-19 12:25:49 +0800

    fix eureka-jenkinsfile

--- vars/springCloudPipeline.groovy
+++ vars/springCloudPipeline.groovy
@@ -31,7 +31,7 @@ def call(Map pipelineParams) {
                             }]
                         }
 
-                        stages parallelStagesMap
+                        parallelStagesMap
                     }
                 }
             }
